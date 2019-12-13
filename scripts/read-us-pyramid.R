84441058fd451933b9c0fb0a6e6371fc176f1129
Zhian N. Kamvar <zkamvar@gmail.com>
2019-12-13 11:06:13 +0000

    add sources for and save it in package

--- scripts/read-us-pyramid.R
+++ scripts/read-us-pyramid.R
@@ -13,10 +13,10 @@
 #'   [np] refers to the regex of n (indicating counts) or p (indicating percentages)
 #'
 #' @return a long table of US census figures by age and sex
-process_pyramids <- function(age_table, counts, what = "gender") {
+process_pyramids <- function(age_table, counts, what = "gender", total = TRUE) {
   WHAT <- rlang::sym(what)
   # Bind together the ages and the genders
-  dplyr::bind_cols(age_table, counts) %>%
+  res <- dplyr::bind_cols(age_table, counts) %>%
     # Process the ages for better labels
     dplyr::mutate(age = dplyr::case_when(
       stringr::str_detect(age, "Under") ~ stringr::str_replace_all(age, ".?Under (\\d+?) years", "<\\1"),
@@ -35,6 +35,13 @@ process_pyramids <- function(age_table, counts, what = "gender") {
     # clean up types
     dplyr::mutate(count = as.integer(count)) %>%
     dplyr::mutate(!!WHAT := forcats::fct_inorder(!!WHAT))
+  if (total) {
+    return(res)
+  }
+
+  res %>%
+    dplyr::filter(age != "total") %>%
+    dplyr::mutate(age = forcats::fct_drop(age))
 }
 
 download_maybe <- function(path) {
@@ -59,13 +66,15 @@ download_maybe <- function(path) {
 #' @param path path to a file
 #' @param full boolean to get full five-year incremental dat or summarized
 #'
-#' @return a long census table`
+#' @return a long census table
 #'
 #' @examples
-#' s <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2018/age-sex-composition/2018gender_table1.xls'
-#' get_simple_pyramid(s, full = TRUE)
+#' s2018 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2018/age-sex-composition/2018gender_table1.xls'
+#' s2008 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2008/age-sex-composition/2008gender_table1.xls'
+#' us_2018 <- get_simple_pyramid(s2018, full = TRUE)
+#' us_2008 <- get_simple_pyramid(s2008, full = TRUE)
 #' get_simple_pyramid(s, full = FALSE)
-get_simple_pyramid <- function(path, full = TRUE) {
+get_simple_pyramid <- function(path, full = TRUE, total = FALSE) {
   path     <- download_maybe(path)
   the_cols <- c('male_n', 'male_p', 'female_n', 'female_p')
 
@@ -76,7 +85,7 @@ get_simple_pyramid <- function(path, full = TRUE) {
   age_table <- readxl::read_excel(path, 
                                   range = if (full) "A7:A25" else "A27:A32", 
                                   col_names = "age")
-  process_pyramids(age_table, np)
+  res <- process_pyramids(age_table, np, what = "gender", total)
 
 }
 
@@ -88,9 +97,17 @@ get_simple_pyramid <- function(path, full = TRUE) {
 #' @return a long census table
 #'
 #' @examples
-#' x <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2018/age-sex-composition/2018gender_table12.xls'
-#' generation <- get_stratified_pyramid(x, what = "Generation")
-get_stratified_pyramid <- function(path, what = "Generation") {
+#' g2018 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2018/age-sex-composition/2018gender_table12.xls'
+#' g2008 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2008/age-sex-composition/2008gender_table27.xls'
+#' us_gen_2018 <- get_stratified_pyramid(g2018, what = "generation")
+#' us_gen_2008 <- get_stratified_pyramid(g2008, what = "generation")
+#' 
+#' i2018 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2018/age-sex-composition/2018gender_table14.xls'
+#' i2008 <- 'https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2008/age-sex-composition/2008gender_table29.xls'
+#'
+#' us_ins_2018 <- get_stratified_pyramid(i2018, what = "insured")
+#' us_ins_2008 <- get_stratified_pyramid(i2008, what = "insured")
+get_stratified_pyramid <- function(path, what = "Generation", total = FALSE) {
   path    <- download_maybe(path)
   males   <- cellranger::cell_limits(c(28, 4), c(46, NA))
   females <- cellranger::cell_limits(c(48, 4), c(66, NA))
@@ -103,11 +120,11 @@ get_stratified_pyramid <- function(path, what = "Generation") {
 
   male_np        <- readxl::read_excel(path, range = males, col_names = categories)
   male_age_table <- readxl::read_excel(path, range = "A28:A46", col_names = "age")
-  male_table     <- process_pyramids(male_age_table, male_np, what = what)
+  male_table     <- process_pyramids(male_age_table, male_np, what = what, total)
 
   female_np        <- readxl::read_excel(path, range = females, col_names = categories)
   female_age_table <- readxl::read_excel(path, range = "A48:A66", col_names = "age")
-  female_table     <- process_pyramids(female_age_table, female_np, what = what)
+  female_table     <- process_pyramids(female_age_table, female_np, what = what, total)
 
   dplyr::bind_rows(male = male_table, female = female_table, .id = "gender") %>%
     dplyr::arrange(age, !!rlang::sym(what), desc(gender)) %>%
